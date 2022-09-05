e1b11eebf956288b6141ebb1f73a1d6d45983389
Ram Prabesh <ramprabeshchauhan43@gmail.com>
2022-09-06 00:18:32 +0530

    v2

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::UsersController < ApplicationController
 
+    before_action :getUser, only: [:showUser, :updateUser, :deleteUser]
+
+    # ─── Get ────────────────────────────────────────────────────────────────────────
     def getUsers
+        user = User.all
+        if user
+            render json: user, status: :ok
+        else
+            render json: {message: "User Empty"}, status: :unprocessable_entity
+        end
 
     end
 
+    # ─── Post ───────────────────────────────────────────────────────────────────────
     def addUser
         user = User.new(username: params[:username], email: params[:email], password_digest: params[:password])
 
@@ -14,6 +24,48 @@ class Api::V1::UsersController < ApplicationController
         end
         
     end 
+
+    # ─── Show ───────────────────────────────────────────────────────────────────────
+    def showUser
+        if @user
+                render json: @user, status: :ok
+        else
+            render json: {message: "User not found.."}, status: :unprocessable_entity
+        end
+    end
+
+    # ─── Put ────────────────────────────────────────────────────────────────────────
+    def updateUser
+        if @user
+            if @user.update(username: params[:username], email: params[:email], password_digest: params[:password])
+                render json: @user, status: :ok
+            else
+                render json: {message: "User failed"}, status: :unprocessable_entity
+            end
+        else
+            render json: {message: "User not found.."}, status: :unprocessable_entity
+        end
+    end
+    
+
+    # ─── Delete ─────────────────────────────────────────────────────────────────────
+    def deleteUser
+        if @user
+            if @user.update(username: params[:username], email: params[:email], password_digest: params[:password])
+                render json: {message: "User deleted"}, status: :ok
+            else
+                render json: {message: "User failed"}, status: :unprocessable_entity
+            end
+        else
+            render json: {message: "User not found.."}, status: :unprocessable_entity
+        end
+    end
+    
+
+    private
+        def getUser
+            @user = User.find(params[:id])
+        end
     
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      post 'add_user', action: :addUser, controller: :users_controller
+
+      get "get_users", action: :getUsers, controller: :users
+      post 'add_user', action: :addUser, controller: :users
+      get "show_user", action: :showUser, controller: :users
+      put "update_user", action: :updateUser, controller: :users
+      delete "delete_user", action: :deleteUser, controller: :users
+
     end
   end
   
